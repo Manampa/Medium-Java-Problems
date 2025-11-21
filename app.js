441b2b68eb1123e1c9b0cfb952808c6ce50445a4
Marc <billpayingmarc@gmail.com>
2025-11-21 10:26:55 -0500

    Q1

---
+++ app.js
@@ -0,0 +1,22 @@
+// Q1 my attempt below wrong
+// function falsyOrTruthy(ele1, ele2) {
+//     if (ele1 === !!0) || (ele2 === !!0) {
+//         return
+//     }
+//     else {
+//         return 
+//     }
+// }
+// console.log(falsyOrTruthy(true, dog))
+// Q1 my attempt above wrong
+
+// Q1 correct code below
+// function falsyOrTruthy(ele1,ele2) {
+    //     return !ele1 ? ele1 : ele2
+    // }
+    
+    // console.log(falsyOrTruthy(true,'dog'))
+    // Q1 correct code above
+
+
+    // Q2 my attempt below
