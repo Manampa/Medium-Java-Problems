4ec98200173c46d32c2784acab9b0c142e277ff3
Marc <billpayingmarc@gmail.com>
2025-11-21 15:16:58 -0500

    Q3

--- app.js
+++ app.js
@@ -19,4 +19,22 @@
     // Q1 correct code above
 
 
-    // Q2 my attempt below
+    // Q2 my attempt not exactly correct below
+    // function arrLength(arr) {
+    //     return arr
+    // }
+    // console.log(arrLength([1,2,3,4]))
+    // Q2 my attempt not exactly correct above
+
+    // Q2 correct code below
+    // function arrLength(arr) {
+    //     return arr.length
+    // }
+    // console.log(arrLength([1,2,3,4]))
+    // Q2 correct code above 
+
+    // Q3 my attempt below
+    function lastEle(arr) {
+        return arr[arr.length-1];
+    } 
+    console.log(lastEle([3,2,0,6,2]))
