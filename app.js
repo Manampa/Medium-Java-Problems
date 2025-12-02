f14eff87c5b2ac15899b75fd855e883662d70063
Marc <billpayingmarc@gmail.com>
2025-12-02 16:49:03 -0500

    Q10

--- app.js
+++ app.js
@@ -167,10 +167,16 @@
 
 // Q10 solution 1 for loop below
 function removeApples(arr) {
-    for (let i = 0; i < arr.length; ++i)
+    let noApples = []
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] !== 'apples'){
+            noApples.push(arr[i])
+        }
+    }
+    return noApples;
 
 }
-console.log*removeApples(['banana', 'oranges', 'apples'])
+console.log(removeApples(['banana', 'oranges', 'apples']))
 // Q10 solution 1 for loop above
 
 
