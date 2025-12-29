59dcf9197eca5c03ff43f882c8d143dbb2b0c197
Marc <billpayingmarc@gmail.com>
2025-12-29 15:10:30 -0500

    Q12

--- app.js
+++ app.js
@@ -195,10 +195,18 @@
 
 // Q11 for loop below
 function filterOutFalsy(arr) {
+    let truthyArr = []
     for (let i = 0; i < arr.length; ++i) {
-        if (arr[i] )
+        if(!!arr[i] === true) {
+            truthyArr.push(arr[i])
+        }
     }
+    return truthyArr;
 }
+console.log(filterOutFalsy(['',NaN, 500, null, false, 0]))
+// Q11 for loop above
+
+// Q12 below 
 
 
 
